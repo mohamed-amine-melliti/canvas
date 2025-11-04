46459f37e99eba845844133132dbaf28bb320829
mohamed-amine-melliti <48659471+mohamed-amine-melliti@users.noreply.github.com>
2025-11-04 10:56:40 +0000

    Deploying to gh-pages from @ mohamed-amine-melliti/canvas@c56c8fcf77edadc50b01e92387604530c15fc1ff 🚀

---
+++ _nuxt/DWQAD4RS.js
@@ -0,0 +1,1 @@
+import{r as t}from"./BpXQVZrc.js";import e from"./Oghn-b9f.js";import{x as n,G as s,d as a,w as i,E as p}from"./CSL-8eSm.js";import"./iik6CYzq.js";const d={__name:"ProseNote",setup(c){const o=n();return(r,f)=>(a(),s(e,{color:"info",icon:p(o).ui.icons.info},{default:i(()=>[t(r.$slots,"default",{mdcUnwrap:"p"})]),_:3},8,["icon"]))}};export{d as default};
---
+++ _nuxt/DenZfxyG.js
@@ -0,0 +1,1 @@
+import{r as t}from"./BpXQVZrc.js";import a from"./Oghn-b9f.js";import{x as e,G as n,d as s,w as c,E as i}from"./CSL-8eSm.js";import"./iik6CYzq.js";const d={__name:"ProseCaution",setup(p){const o=e();return(r,f)=>(s(),n(a,{color:"error",icon:i(o).ui.icons.caution},{default:c(()=>[t(r.$slots,"default",{mdcUnwrap:"p"})]),_:3},8,["icon"]))}};export{d as default};
